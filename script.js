12f25d84d258223e7e2c3d46735db79a71f96d70
Jalal <iamjalalhamed@gmail.com>
2022-02-23 17:04:02 +0330

    create pointer

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const c = canvas.getContext('2d');
 canvas.height = innerHeight;
 canvas.width = innerWidth;
 
-const ballColor = '#1f73f2';
-const brickColor = '#ef4921';
+const colors = {
+  ball: '#1f73f2',
+  brick: '#ef4921',
+  pointer: '#4ea0ed',
+};
 
 let round = 1;
 
@@ -21,9 +24,9 @@ class Ball {
   draw() {
     c.beginPath();
     c.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
-    c.fillStyle = ballColor;
+    c.fillStyle = colors.ball;
     c.fill();
-    c.strokeStyle = ballColor;
+    c.strokeStyle = colors.ball;
     c.stroke();
   }
 }
@@ -42,6 +45,7 @@ class Border {
   }
 
   draw() {
+    c.fillStyle = '#000';
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 }
@@ -50,9 +54,7 @@ class Brick {
   constructor() {
     this.position = {
       x: 100,
-      // must be greater than the topBorder and less than
-      // the bottomBorder's y position + the borders' heights
-      y: 300,
+      y: 300, // must be greater than the topBorder and less than the bottomBorder's y position + the borders' heights
     };
 
     this.width = 100;
@@ -60,9 +62,9 @@ class Brick {
   }
 
   draw() {
-    c.fillStyle = brickColor;
+    c.fillStyle = colors.brick;
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
-    c.font = `30px sans`;
+    c.font = `30px play`;
     c.fillStyle = '#fff';
     c.textAlign = 'center';
     c.textBaseline = 'middle';
@@ -75,27 +77,65 @@ class Brick {
 }
 
 class Pointer {
-  constructor() {
+  constructor(x, y) {
     this.position = {
-      x: 0,
-      y: 0,
+      x: ball.position.x,
+      y: ball.position.y - ball.radius,
     };
 
-    this.width = innerWidth;
-    this.height = 5;
+    this.pointTo = {
+      x: x,
+      y: y,
+    };
+
+    this.width = 5;
+    this.height = innerWidth;
   }
 
-  draw() {}
+  draw() {
+    c.beginPath();
+    c.moveTo(ball.position.x, ball.position.y);
+    c.lineTo(this.pointTo.x, this.pointTo.y);
+    c.closePath();
+    c.fill();
+    c.strokeStyle = colors.pointer;
+    c.lineWidth = 5;
+    c.stroke();
+  }
+
+  clear() {
+    c.clearRect(0, 0, innerWidth, innerHeight);
+  }
 }
 
 const topBorder = new Border(200);
 const bottomBorder = new Border(innerHeight - 200);
 const brick = new Brick();
 const ball = new Ball();
-const pointer = new Pointer();
 
-topBorder.draw();
-bottomBorder.draw();
-brick.draw();
-ball.draw();
-pointer.draw();
+function paintGame() {
+  topBorder.draw();
+  bottomBorder.draw();
+  brick.draw();
+  ball.draw();
+}
+
+paintGame();
+
+addEventListener('mousemove', e => {
+  const pointer = new Pointer(e.x, e.y);
+  if (
+    e.y > topBorder.position.y + topBorder.height &&
+    e.y < bottomBorder.position.y + bottomBorder.height
+    /* && the ball is not moving */
+  ) {
+    pointer.clear();
+    paintGame();
+    pointer.draw();
+    canvas.style.cursor = 'pointer';
+  } else {
+    pointer.clear();
+    paintGame();
+    canvas.style.cursor = 'auto';
+  }
+});
