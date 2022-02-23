891cc936759f813bf14571daf96113c7439b7e40
Jalal <iamjalalhamed@gmail.com>
2022-02-23 10:55:44 +0330

    switching to cnavas

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('works');
+// DOM elements
+const td = document.querySelector('td');
+const canvas = document.createElement('canvas');
+
+class Game {
+  constructor() {
+    this._setSize();
+    window.addEventListener('resize', this._setSize);
+
+    this.ctx = canvas.getContext('2d');
+    this._renderGameField();
+  }
+
+  _setSize() {
+    canvas.setAttribute('width', window.innerWidth);
+    canvas.setAttribute('height', window.innerHeight);
+  }
+
+  _renderGameField() {
+    console.log(this.ctx);
+    this.ctx.beginPath();
+    this.ctx.moveTo(0, 50);
+    this.ctx.lineTo(window.innerWidth, 50);
+    this.ctx.stroke();
+  }
+}
+
+const game = new Game();
+td.appendChild(canvas);
