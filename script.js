89ca3b0c93fa55eb6a6692d1821def60434e5b58
Jalal <iamjalalhamed@gmail.com>
2022-02-24 10:08:07 +0330

    create pointer ball, abbreviate 'position' to pos and 'radius' to r

--- script.js
+++ script.js
@@ -15,16 +15,17 @@ let round = 1;
 
 class Ball {
   constructor() {
-    this.radius = 12;
-    this.position = {
+    this.r = 12;
+    this.pos = {
       x: innerWidth / 2,
-      y: bottomBorder.position.y - this.radius,
+      y: bottomBorder.pos.y - this.r,
     };
   }
 
   draw() {
     c.beginPath();
-    c.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
+    c.setLineDash([]);
+    c.arc(this.pos.x, this.pos.y, this.r, 0, 2 * Math.PI);
     c.fillStyle = colors.ball;
     c.fill();
     c.strokeStyle = colors.ball;
@@ -34,12 +35,12 @@ class Ball {
 }
 
 class Border {
-  constructor(yPosition) {
-    this.yPosition = yPosition;
+  constructor(ypos) {
+    this.ypos = ypos;
 
-    this.position = {
+    this.pos = {
       x: 0,
-      y: this.yPosition,
+      y: this.ypos,
     };
 
     this.width = innerWidth;
@@ -48,15 +49,15 @@ class Border {
 
   draw() {
     c.fillStyle = '#000';
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    c.fillRect(this.pos.x, this.pos.y, this.width, this.height);
   }
 }
 
 class Brick {
   constructor() {
-    this.position = {
+    this.pos = {
       x: 100,
-      y: 300, // must be greater than the topBorder and less than the bottomBorder's y position + the borders' heights
+      y: 300, // must be greater than the topBorder and less than the bottomBorder's y pos + the borders' heights
     };
 
     this.width = 100;
@@ -65,15 +66,15 @@ class Brick {
 
   draw() {
     c.fillStyle = colors.brick;
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    c.fillRect(this.pos.x, this.pos.y, this.width, this.height);
     c.font = `30px play`;
     c.fillStyle = '#fff';
     c.textAlign = 'center';
     c.textBaseline = 'middle';
     c.fillText(
       round,
-      this.position.x + this.width / 2,
-      this.position.y + this.height / 2
+      this.pos.x + this.width / 2,
+      this.pos.y + this.height / 2
     );
   }
 }
@@ -90,31 +91,49 @@ class Pointer {
   }
 
   get calcEndPoint() {
-    this.pointA = [ball.position.x, ball.position.y];
+    this.result = [];
+    this.pointA = [ball.pos.x, ball.pos.y];
     this.pointB = [this.mouseCoords.x, this.mouseCoords.y];
     this.slope =
       (this.pointA[1] - this.pointB[1]) / (this.pointA[0] - this.pointB[0]);
     this.b = this.mouseCoords.y - this.slope * this.mouseCoords.x;
-    this.x = (topBorder.position.y + topBorder.height - this.b) / this.slope;
+    this.x = (topBorder.pos.y + topBorder.height - this.b) / this.slope;
     this.y = innerWidth * this.slope + this.b;
+
     if (this.slope === Infinity)
-      return [ball.position.x, topBorder.position.y + topBorder.height];
+      this.result = [ball.pos.x, topBorder.pos.y + topBorder.height];
     if (this.x > 0 && this.x < innerWidth)
-      return [this.x, topBorder.position.y + topBorder.height];
-    if (this.x < 0 && this.b < 440) return [0, this.b];
-    if (this.x < 0 && this.b > 440) return [0, 440];
-    if (this.x > innerWidth && this.y < 440) return [innerWidth, this.y];
-    if (this.x > innerWidth && this.y > 440) return [innerWidth, 440];
+      this.result = [this.x, topBorder.pos.y + topBorder.height];
+    if (this.x < ball.r && this.b < 440) this.result = [ball.r, this.b];
+    if (this.x < ball.r && this.b > 440) this.result = [ball.r, 440];
+    if (this.x > innerWidth - ball.r && this.y < 440)
+      this.result = [innerWidth - ball.r, this.y];
+    if (this.x > innerWidth - ball.r && this.y > 440)
+      this.result = [innerWidth - ball.r, 440];
+
+    return [this.result[0], this.result[1] + ball.r];
   }
 
   draw() {
+    // Dashed path
     c.beginPath();
-    c.moveTo(ball.position.x, ball.position.y);
+    c.setLineDash([10, 10]);
+    c.moveTo(ball.pos.x, ball.pos.y);
     c.lineTo(...this.calcEndPoint);
     c.closePath();
     c.fill();
     c.strokeStyle = colors.pointer;
     c.lineWidth = 5;
+    c.stroke();
+
+    // Pointer ball
+    c.beginPath();
+    c.setLineDash([]);
+    c.arc(...this.calcEndPoint, ball.r, 0, 2 * Math.PI);
+    c.fillStyle = colors.pointer;
+    c.fill();
+    c.strokeStyle = colors.pointer;
+    c.lineWidth = 1;
     c.stroke();
   }
 
@@ -127,9 +146,9 @@ class Coefficient {
   constructor() {
     this.coefficient = 1;
 
-    this.position = {
-      x: ball.position.x,
-      y: ball.position.y + bottomBorder.height + ball.radius * 3,
+    this.pos = {
+      x: ball.pos.x,
+      y: ball.pos.y + bottomBorder.height + ball.r * 3,
     };
   }
 
@@ -137,7 +156,7 @@ class Coefficient {
     c.font = '30px play';
     c.fillStyle = colors.ball;
     c.textAlign = 'center';
-    c.fillText(`x${this.coefficient}`, this.position.x, this.position.y);
+    c.fillText(`x${this.coefficient}`, this.pos.x, this.pos.y);
   }
 }
 
@@ -157,12 +176,13 @@ function paintGame() {
 
 paintGame();
 
-addEventListener('mousemove', e => {
+// Functions
+const handlePointer = e => {
   const pointer = new Pointer(e.x, e.y);
 
   if (
-    e.y > topBorder.position.y + topBorder.height &&
-    e.y < bottomBorder.position.y - bottomBorder.height - ball.radius
+    e.y > topBorder.pos.y + topBorder.height &&
+    e.y < bottomBorder.pos.y - bottomBorder.height - ball.r
     /* && the ball is not moving */
   ) {
     pointer.clear();
@@ -176,4 +196,8 @@ addEventListener('mousemove', e => {
     canvas.style.cursor = 'auto';
     if (!out) out = true;
   }
-});
+};
+
+// Event Listeners
+addEventListener('mousemove', handlePointer);
+addEventListener('resize', paintGame);
