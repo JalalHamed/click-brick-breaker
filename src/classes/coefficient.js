6f9f036b065133de64abd4ec87467743a014c5da
Jalal <iamjalalhamed@gmail.com>
2022-04-12 12:00:45 +0430

    remove MAX_COEFFICIENT_Y_POS since it would brick in different screens (will get back to it later)

--- src/classes/coefficient.js
+++ src/classes/coefficient.js
@@ -7,7 +7,6 @@ import {
   CANVAS,
   SIZES,
   COLORS,
-  MAX_COEFFICIENT_Y_POS as M_C_Y_P,
   MAX_COEFFICIENT_FONT_SIZE as M_C_F_S,
 } from '../config.js';
 // State
@@ -17,11 +16,9 @@ class Coefficient {
   constructor() {
     this.count = state.getLS('coefficient') || 1;
 
-    this.posY = bottomBorder.heightFromTop + mainBall.r * 2.5;
-
     this.pos = {
       x: mainBall.pos.x,
-      y: this.posY < M_C_Y_P ? this.posY : M_C_Y_P,
+      y: bottomBorder.heightFromTop + mainBall.r * 2.5,
     };
   }
 
@@ -44,9 +41,8 @@ class Coefficient {
   }
 
   repoSize() {
-    this.posY = bottomBorder.heightFromTop + mainBall.r * 2.5;
     this.pos.x = mainBall.pos.x;
-    this.pos.y = this.posY < M_C_Y_P ? this.posY : M_C_Y_P;
+    this.pos.y = bottomBorder.heightFromTop + mainBall.r * 2.5;
   }
 }
 
