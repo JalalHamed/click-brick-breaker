c24885df6b48fbbd3aafad169263be5cc22cd2b3
Jalal <iamjalalhamed@gmail.com>
2022-04-17 18:48:59 +0430

    refactor bonus and fix it's repoSize

--- src/classes/Bonus.js
+++ src/classes/Bonus.js
@@ -9,8 +9,7 @@ export default class Bonus {
   constructor(props) {
     this.props = props;
     this.isGoingDown = true;
-    this.r = SIZES.ball.radius;
-    this.ringR = SIZES.ball.radius;
+    this.ring = SIZES.ball.radius;
 
     this.pos = {
       x: state.grid.row[props.gridRowIndex] + SIZES.brick.width / 2,
@@ -21,36 +20,37 @@ export default class Bonus {
     };
   }
 
-  ringRadius() {
-    if (this.ringR > this.r && this.isGoingDown) this.ringR--;
-    if (this.ringR === this.r) this.isGoingDown = false;
-    if (this.ringR < this.r * 2 && !this.isGoingDown) this.ringR++;
-    if (this.ringR === this.r * 2) this.isGoingDown = true;
+  swingRing() {
+    const { radius } = SIZES.ball;
+    if (this.ring > radius && this.isGoingDown) this.ring--;
+    if (this.ring === radius) this.isGoingDown = false;
+    if (this.ring < radius * 2 && !this.isGoingDown) this.ring++;
+    if (this.ring === radius * 2) this.isGoingDown = true;
   }
 
   draw() {
     // bonus ball
     C.beginPath();
     C.setLineDash([]);
-    C.arc(this.pos.x, this.pos.y, this.r, 0, 2 * Math.PI);
+    C.arc(this.pos.x, this.pos.y, SIZES.ball.radius, 0, 2 * Math.PI);
     C.fillStyle = COLORS.ball.bonus;
     C.fill();
 
     // bonus ball's ring
     C.beginPath();
     C.setLineDash([]);
-    C.arc(this.pos.x, this.pos.y, this.ringR, 0, 2 * Math.PI);
+    C.arc(this.pos.x, this.pos.y, this.ring, 0, 2 * Math.PI);
     C.lineWidth = SIZES.border.height / 1.3;
     C.strokeStyle = COLORS.ball.bonus;
     C.stroke();
 
     // bonus ball's ring animation
-    if (state.counter % 5 === 0) this.ringRadius();
+    if (state.counter % 3 === 0) this.swingRing();
   }
 
   repoSize() {
     this.pos = {
-      x: state.grid[this.props.gridRowIndex] + SIZES.brick.width / 2,
+      x: state.grid.row[this.props.gridRowIndex] + SIZES.brick.width / 2,
       y:
         topBorder.heightFromTop +
         SIZES.brick.height / 2 +
