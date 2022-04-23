d877d616348c3dccd98c0f9a0672101c2a29a893
Jalal <iamjalalhamed@gmail.com>
2022-04-23 12:12:05 +0430

    slight changes in swingBonusRing

--- src/functions/animations/swingBonusRing.js
+++ src/functions/animations/swingBonusRing.js
@@ -8,10 +8,10 @@ let isGoingDown = false;
 const swingBonusRing = () => {
   const { radius } = SIZES.ball;
   if (state.bonusRing > radius && isGoingDown) state.bonusRing--;
-  if (state.bonusRing === radius) isGoingDown = false;
+  if (state.bonusRing <= radius) isGoingDown = false;
   if (state.bonusRing < Math.floor(radius * 1.7) && !isGoingDown)
-    state.bonusRing++;
-  if (state.bonusRing === Math.floor(radius * 1.7)) isGoingDown = true;
+    state.bonusRing += state.bonusRing / 20;
+  if (state.bonusRing >= Math.floor(radius * 1.7)) isGoingDown = true;
 };
 
 export default swingBonusRing;
