10472f2b23b1077cca2ee0b85daa95e77c1d4097
Jalal <iamjalalhamed@gmail.com>
2022-05-09 13:49:31 +0430

    refactor bringDownBaB and fix the bounce back low velocity

--- src/config.js
+++ src/config.js
@@ -15,15 +15,6 @@ export const CANVAS_MIN_WIDTH = 350;
 export const CANVAS_MIN_HEIGHT = 600;
 export const MIN_PARTICLE_RADIUS = 6;
 export const BRICKS_MARGIN = 3;
-export const MAX_ANGLE = 2.96706; // 2.96706 radiance = 170 degrees
-export const MIN_ANGLE = 0.174533; // 0.174533 radiance = 10 degrees
-export const SAFE_MARGIN_FROM_CANVAS_SIDES = 0.1; // so it won't get buggy and stuck.
-export const INCRESCENT_DISTANCE_TO_TAKE = 150;
-export const PROJECTILE_VELOCITY_COEFFICIENT = 15;
-export const MERGING_VELOCITY = 20;
-export const BONUS_RING_MIN_ADD = 0.4;
-export const EMITTED_PROJECTILES_MARGIN = innerWidth < 700 ? 2 : 3;
-export const PIECE_DISTANCE_TO_TAKE_BEFORE_FADE = getBorderMargin() * 2;
 
 CANVAS.height =
   innerHeight >= CANVAS_MIN_HEIGHT ? innerHeight : CANVAS_MIN_HEIGHT;
@@ -49,6 +40,16 @@ export const SIZES = {
     ring: { min: getParticleRadius(), max: getParticleRadius() * 1.8 },
   },
   piece: { width: getBrickWidth() / 6, height: getBrickHeight() / 4 },
-  BaB_bounce: getBrickHeight() / 2,
   font: getFontSize(),
 };
+
+export const MAX_ANGLE = 2.96706; // 2.96706 radiance = 170 degrees
+export const MIN_ANGLE = 0.174533; // 0.174533 radiance = 10 degrees
+export const SAFE_MARGIN_FROM_CANVAS_SIDES = 0.1; // so it won't get buggy and stuck.
+export const INCRESCENT_DISTANCE_TO_TAKE = 150;
+export const PROJECTILE_VELOCITY_COEFFICIENT = 15;
+export const MERGING_VELOCITY = 20;
+export const BONUS_RING_MIN_ADD = 0.4;
+export const EMITTED_PROJECTILES_MARGIN = innerWidth < 700 ? 2 : 3;
+export const PIECE_DISTANCE_TO_TAKE_BEFORE_FADE = getBorderMargin() * 2;
+export const BRICKS_AND_BONUSES_BOUNCE_SIZE = getBrickHeight() / 2;
--- src/functions/motions/bringDownBaB.js
+++ src/functions/motions/bringDownBaB.js
@@ -1,24 +1,24 @@
 // Configs
-import { SIZES } from '../../config.js';
+import { BRICKS_AND_BONUSES_BOUNCE_SIZE as B_A_B_B_S } from '../../config.js';
 // State
 import state from '../../state.js';
 
 let isGoingDown = true;
 
 const bringDownBricksAndBonuses = () => {
-  const items = [
+  const velocity = 5;
+  const BricksAndBonuses = [
     ...state.bricks,
     ...state.bonuses.filter(bonus => bonus.mode === 'stable'),
   ];
 
-  items.forEach(item => {
-    const posY = Math.round(item.pos.y);
-    const posNextY = Math.round(item.pos.nextY);
-
-    if (posY < posNextY + SIZES.BaB_bounce && isGoingDown) item.pos.y += 5;
-    if (posY >= posNextY + SIZES.BaB_bounce && isGoingDown) isGoingDown = false;
-    if (posY > posNextY && !isGoingDown) item.pos.y -= 1;
-    if (posY === posNextY && !isGoingDown) {
+  BricksAndBonuses.forEach(item => {
+    if (item.pos.y < item.pos.nextY + B_A_B_B_S && isGoingDown)
+      item.pos.y += velocity;
+    else if (item.pos.y - velocity > item.pos.nextY) {
+      item.pos.y -= velocity;
+      isGoingDown = false;
+    } else {
       isGoingDown = true;
       state.isBringingDown.bricks = false;
       state.isBringingDown.bonuses = false;
@@ -26,7 +26,7 @@ const bringDownBricksAndBonuses = () => {
   });
 
   if (!state.isBringingDown.bricks && !state.isBringingDown.bonuses)
-    items.forEach(item => item.updateYPos());
+    BricksAndBonuses.forEach(item => item.updateYPos());
 };
 
 export default bringDownBricksAndBonuses;
