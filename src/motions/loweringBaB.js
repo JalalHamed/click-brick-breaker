3c50cc1fcbf5c9110cfa4dabd6c8c518454e1513
Jalal <iamjalalhamed@gmail.com>
2022-05-15 11:39:38 +0430

    refactor and fix changing bricks colors on lowering

--- src/motions/loweringBaB.js
+++ src/motions/loweringBaB.js
@@ -5,49 +5,50 @@ import { convertRGBtoArr, getColorsDifferences } from '../helpers.js';
 import state from '../state.js';
 
 let isGoingDown = true;
-let hasCalculatedSteps = false;
+let differences = {};
 let steps;
 
 const loweringBricksAndBonuses = () => {
-  const velocity = 7;
+  const velocity = 5;
   const bricksAndBonuses = [...state.bricks, ...state.bonuses].filter(
     item => item.mode === 'lower'
   );
 
+  if (!Object.keys(differences).length)
+    state.bricks.forEach(brick => {
+      differences[brick.id] = getColorsDifferences(
+        brick.color,
+        brick.updateColor()
+      );
+    });
+
   bricksAndBonuses.forEach(item => {
-    if (!hasCalculatedSteps && item.constructor.name === 'Brick') {
+    if (!steps && item.constructor.name === 'Brick')
       steps = Math.ceil((item.pos.nextY + B_A_B_B_S - item.pos.y) / velocity);
-      hasCalculatedSteps = true;
-    }
 
     if (item.pos.y < item.pos.nextY + B_A_B_B_S && isGoingDown) {
       item.pos.y += velocity;
+
       // Update color
       if (item.constructor.name === 'Brick') {
         const currColor = convertRGBtoArr(item.color);
-        console.log(currColor);
-        const differences = getColorsDifferences(
-          item.color,
-          item.updateColor()
-        );
-        item.color = `rgb(255, ${currColor[1] + differences[1] / steps}, ${
-          currColor[2] + differences[2] / steps
-        })`;
+        item.color = `rgb(255, ${
+          currColor[1] + differences[item.id][1] / steps
+        }, ${currColor[2] + differences[item.id][2] / steps})`;
       }
     } else if (item.pos.y - velocity > item.pos.nextY) {
       item.pos.y -= velocity;
       isGoingDown = false;
-    } else {
-      if (item.constructor.name === 'Brick') item.color = item.updateColor();
-      isGoingDown = true;
-      hasCalculatedSteps = false;
-      steps = null;
-      bricksAndBonuses.forEach(item => (item.mode = 'stable'));
-    }
+      state.bricks.forEach(brick => (brick.color = brick.updateColor()));
+    } else bricksAndBonuses.forEach(item => (item.mode = 'stable'));
   });
 
-  if (bricksAndBonuses.every(item => item.mode === 'stable'))
+  if (bricksAndBonuses.every(item => item.mode === 'stable')) {
     bricksAndBonuses.forEach(item => item.updateYPos());
+    isGoingDown = true;
+    steps = null;
+    differences = {};
+  }
 };
 
 export default loweringBricksAndBonuses;
